9095f569b9b8b10eefa19e1e5dd7e9f519630692
FIXED-TERM Lu Xu (CR/PJ-AI-E) <lux1rng@bosch.com>
2022-02-15 09:22:03 +0100

    add post request

--- src/components/forms/meetupform.js
+++ src/components/forms/meetupform.js
@@ -1,17 +1,36 @@
+import {useRef} from 'react'
+
 import classes from './meetupform.module.css'
 import Card from '../wrapper/Card';
 
-function Meetupform()
+function Meetupform(props)
 {
+    const titleref = useRef();
+    const imageref = useRef();
+
+    function submitHandler(event)
+    {
+        event.preventDefalut();
+        const entertitle = titleref.current.value; 
+        const enterimage = imageref.current.value; 
+
+        const data = {
+            title: entertitle,
+            image: enterimage
+        }
+
+        props.update(data);
+    }
+
     return <Card>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='title'>Meetup title</label>
-                <input id='title' required type='text' />
+                <input id='title' required type='text' ref={titleref} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='image'>Meetup image</label>
-                <input id='image' required type='url' />
+                <input id='image' required type='url' ref={imageref}/>
             </div>
             <div className={classes.actions}>
                 <button> Add </button>
--- src/routes/Newmeetup.js
+++ src/routes/Newmeetup.js
@@ -2,9 +2,23 @@ import Layout from "../components/layout/layout";
 import Meetupform from "../components/forms/meetupform";
 function Newmeetup(){
 
+    function update(data)
+    {
+        fetch(
+            'url should be set after',
+            {
+                method:'POST', 
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application-json'
+                }
+            }
+        );
+    }
+
     return <section>
                 <h1>New Meetup</h1>
-                <Meetupform />
+                <Meetupform update={update}/>
             </section>;
 }
 
